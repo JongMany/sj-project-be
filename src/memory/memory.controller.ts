39294cea868e4a0b576ed54005e2d52c266661e3
JongMany <blackberry1114@naver.com>
2024-10-24 21:20:48 +0900

    feat: memory data api

--- src/memory/memory.controller.ts
+++ src/memory/memory.controller.ts
@@ -10,7 +10,6 @@ export class MemoryController {
   @UseGuards(JwtAuthGuard)
   async getMemories(@Param('threadId') threadId: string, @Response() res) {
     const memories = await this.memoryService.getMemoriesByThreadId(threadId);
-    console.log('memories', memories);
     return res.json({
       memories: memories,
       success: true,
